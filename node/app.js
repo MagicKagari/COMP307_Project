96cc9f79c517454003ea40a87eecdaa68614acca
MagicKagari <gaoran1994@gmail.com>
2015-11-14 19:38:56 -0500

    init of node environment

---
+++ node/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var app = express();
+
+
+app.get('/', function(req,resp){
+    resp.sendFile(path.join(__dirname+'/../html/index.html'));
+});
+
+var server = app.listen(1337, function(){
+    console.log('Starting server');
+});
