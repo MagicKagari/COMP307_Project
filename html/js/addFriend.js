df96ca435dcc376979561fa78007211dad9cf44b
Seonah Han <seonah.han123@gmail.com>
2015-11-25 21:25:17 -0500

    2 javascript files

---
+++ html/js/addFriend.js
@@ -0,0 +1,30 @@
+function checkit(){
+	
+	var username = document.getElementById("username").value;
+	var friendname = document.getElementById("friendname").value;
+	
+	var Friend = new Object();
+		Friend.friendname = friendname;
+		Friend.username=username;
+		jsonString = JSON.stringify(Friend);
+	
+		xhttp = new XMLHttpRequest();
+  			xhttp.onreadystatechange = function() {
+  				//TO GET SHAREDKEY 
+    			if (xhttp.readyState == 4 && xhttp.status == 200) {
+      				 var check= xhttp.responseText;
+      				 if(check==0){
+      				 	//SUCCESFULLY ADDED "THE FRIEND NAME" 
+      				 }
+      				 else{
+      				 	//"THE FRIEND NAME" NOT FOUND
+      				 }
+
+					}
+  			}
+  		xhttp.open("POST", "~/COMP307_Project/php/addFriend.php", true);
+  		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhttp.send("jsonUser="+jsonString);
+  		xhttp.send();
+	}
+
---
+++ html/js/loginCheck.js
@@ -0,0 +1,87 @@
+
+function checkit(){
+	
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+
+	var sharedKey= 0;
+	var sessionID= 0;
+
+	var user = new Object();
+		user.username = username;
+		jsonString = JSON.stringify(user);
+	
+		xhttp = new XMLHttpRequest();
+  			xhttp.onreadystatechange = function() {
+  				//TO GET SHAREDKEY 
+    			if (xhttp.readyState == 4 && xhttp.status == 200) {
+      				sharedKey = xhttp.responseText;
+
+					var encryptedPassword = encrypt(password, sharedKey);
+
+					var account = new Object();
+						account.username = username;
+						account.password = encryptedPassword;
+						jsonString = JSON.stringify(account);
+				
+						xhttp = new XMLHttpRequest();
+  							xhttp.onreadystatechange = function() {
+  								//TO GET THE PASSWORD MATCHING RESULT
+    							if (xhttp.readyState == 4 && xhttp.status == 200) {
+      								var check=xhttp.responseText;
+      								
+      								check=parseInt(check);
+      								 //ONCE THE CHECK IS TRUE
+      								if(check==0){
+      										xhttp = new XMLHttpRequest();
+  											xhttp.onreadystatechange = function() {
+  												if (xhttp.readyState == 4 && xhttp.status == 200) {
+  													sessionID=xhttp.responseText;
+  													//sessionID=parseInt(sessionID);	
+  													localStorage.setItem("currentSession", sessionID);
+  													/*Hello, Stanley*/
+  													/*IF THE LOGIN WAS SUCCESFULL*/
+    											}
+
+  											}
+  											xhttp.open("POST", "~/COMP307_Project/php/sessionID.php", true);
+  											xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+											xhttp.send("jsonAccount="+jsonString);
+  											xhttp.send();
+      								}      							
+      								else{
+      									/*Hello, Stanley*/
+      								//IF THE LOGIN WAS NOT SUCCESSFULL
+      								}
+     							}
+  							}
+  						xhttp.open("POST", "~/COMP307_Project/php/test.php", true);
+  						xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+						xhttp.send("jsonAccount="+jsonString);
+  						xhttp.send();
+    			}
+  			}
+  		xhttp.open("POST", "~/COMP307_Project/php/getSharedKey.php", true);
+  		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhttp.send("jsonUser="+jsonString);
+  		xhttp.send();
+	}
+
+
+function encrypt(original, key){
+	var encryptedString = "";
+	var shift=key;
+	for(i = 0; i< original.length;i++){
+		var str= original.charAt(i);
+		var ascii = str.charCodeAt(0);
+		var addition = parseInt(ascii) + parseInt(shift);
+		if(ascii >= 65 && ascii <=90){
+			ascii = ((addition - 65)%26)+65;
+		}
+		else if(ascii >=97 && ascii <= 122){
+			ascii = ((addition - 97)%26)+97;
+		}
+		encryptedString = encryptedString + String.fromCharCode(ascii);
+	}
+	return encryptedString;
+}
